79e2da44e37bbacbcb9171ca79415a4dd7fcbc07
Patricia Vu <p8vu@ucsd.edu>
2018-01-25 10:39:10 -0800

    Made Changes to JS and CSS files

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,13 +6,28 @@ $(document).ready(function() {
 })
 
 /*
- * Function that is called when the document is ready.
+ * Fnuction that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
-	});
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").addClass("active");
 
+	});
+	$("a.thumbnail").click(projectClick)
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	// example: $("#div-id").closestick(functionToCall);
+}
+
+function projectClick(e) {
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       $("project-description").fadeOut()
+    }
 }
